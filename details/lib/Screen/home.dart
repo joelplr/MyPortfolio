71a3ef9defb8c014c4e5a37b0c984133123ad1bd
Joel A Jacob <joeljacobplr@gmail.com>
2025-09-03 17:19:19 +0530

    Circle avathar and two buttons added

--- details/lib/Screen/home.dart
+++ details/lib/Screen/home.dart
@@ -5,6 +5,27 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("About Page"), backgroundColor: Colors.teal),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30),
+            CircleAvatar(radius: 70, backgroundColor: Colors.blue),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {}, child: Text('About Me')),
+                SizedBox(width: 30,),
+                ElevatedButton(onPressed: (){}, child: Text("Contacts"))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
